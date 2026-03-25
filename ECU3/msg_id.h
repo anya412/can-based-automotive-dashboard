4fccc55adc8a0380dcc6e705cf300348d139dd90
Ananya Jayaprakash <ananyajay14@gmail.com>
2026-03-25 21:24:38 +0530

    Minor changes

--- ECU3/msg_id.h
+++ ECU3/msg_id.h
@@ -1,13 +1,11 @@
 #ifndef MSG_ID_H
 #define MSG_ID_H
 
-/*---------------------------------------------------------
- * CAN Message Identifiers
- *---------------------------------------------------------*/
+// CAN Message Identifiers
 #define SPEED_MSG_ID               0x10
 #define GEAR_MSG_ID                0x20
 #define RPM_MSG_ID                 0x30
 #define ENG_TEMP_MSG_ID            0x40
 #define INDICATOR_MSG_ID           0x50
 
-#endif /* MSG_ID_H */
+#endif
