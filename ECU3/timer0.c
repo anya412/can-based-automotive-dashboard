73600479d8d1bb494be4fbb9f409c9ccbb28a85e
Ananya Jayaprakash <ananyajay14@gmail.com>
2026-03-25 20:54:39 +0530

    Update timer0.c

--- ECU3/timer0.c
+++ ECU3/timer0.c
@@ -1,37 +1,37 @@
 #include <xc.h>
 #include "timer0.h"
 
-/*---------------------------------------------------------
+/***********************************************************
  * Function : init_timer0
  * Description :
- *    Configures Timer0 for periodic interrupts.
+ *    Configures Timer 0 for periodic interrupts.
  *
  *    - 8-bit mode
  *    - Internal clock (Fosc / 4)
  *    - Prescaler disabled
  *    - Preload = 6
  *    - Overflow interrupt enabled
- *---------------------------------------------------------*/
+ **********************************************************/
 void init_timer0(void)
 {
-    /* Select 8-bit Timer0 */
+    // Select 8-bit Timer0
     T08BIT = 1;
 
-    /* Clock source = internal instruction cycle clock (Fosc/4) */
+    // Clock source = internal instruction cycle clock (Fosc/4)
     T0CS = 0;
 
-    /* Disable prescaler */
+    // Disable prescaler
     PSA = 1;
 
-    /* Preload initial value */
+    // Preload initial value
     TMR0 = 6;
 
-    /* Clear interrupt flag */
+    // Clear interrupt flag
     TMR0IF = 0;
 
-    /* Enable Timer0 interrupt */
+    // Enable Timer0 interrupt
     TMR0IE = 1;
 
-    /* Start Timer0 */
+    // Start Timer0
     TMR0ON = 1;
 }
